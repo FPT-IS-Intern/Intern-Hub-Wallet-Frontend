4b577e6eb2ef662aea9ee56289bfc749bd708d98
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-03-19 10:51:28 +0700

    chore: add @angular-architects/native-federation as a dev dependency

---
+++ federation.config.js
@@ -0,0 +1,46 @@
+const { withNativeFederation, shareAll } = require('@angular-architects/native-federation/config');
+
+module.exports = withNativeFederation({
+  name: 'lms',
+
+  exposes: {
+    './routes': './src/app/app.routes.ts',
+  },
+
+  shared: {
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    }),
+    '@angular/common': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+      includeSecondaries: true,
+    },
+    '@angular/common/locales/vi': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    'dynamic-ds': {
+      singleton: true,
+      strictVersion: false,
+      requiredVersion: 'auto',
+      includeSecondaries: true,
+    },
+    'ng-zorro-antd': {
+      singleton: true,
+      strictVersion: false,
+      requiredVersion: 'auto',
+      includeSecondaries: true,
+    },
+  },
+
+  skip: ['rxjs/ajax', 'rxjs/fetch', 'rxjs/testing', 'rxjs/webSocket'],
+
+  features: {
+    ignoreUnusedDeps: true,
+  },
+});
